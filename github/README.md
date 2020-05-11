3f3e9f3dcbb23b6fd8be9ae771a681dcc0ff50dc
Admin <odydasa@gmail.com>
2020-05-11 23:14:09 +0700

    Add github repositories

--- github/README.md
+++ github/README.md
@@ -1,8 +1,18 @@
 # Script Repositories from Github
 
-https://github.com/Disassembler0/mikrotik-scripts.git
+;https://github.com/Disassembler0/mikrotik-scripts.git
 
-https://github.com/missinglink/mikrotik-openvpn-client.git
+;https://github.com/missinglink/mikrotik-openvpn-client.git
 
-https://github.com/tarampampam/mikrotik-hosts-parser.git
+;https://github.com/tarampampam/mikrotik-hosts-parser.git
 	
+;https://github.com/massimo-filippi/mikrotik.git
+
+;https://github.com/0ki/mikrotik-tools.git
+
+;https://github.com/HathemAhmed/MikrotikSploit.git
+
+;https://github.com/oh4va/mikrotik-dyndns.git
+
+;https://github.com/jcmturner/ddns.git
+
---
+++ github/make_repo.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+SETLOCAL
+FOR /F %%A IN (README.md) DO CALL :make %%A
+git commit -m "Add github repositories"
+git push
+ENDLOCAL
+GOTO :EOF
+
+:make
+SET _git=%*
+IF NOT DEFINED _git GOTO :EOF
+IF "%_git:~0,1%"=="#" GOTO :EOF
+IF "%_git:~0,1%"==";" GOTO :EOF
+ECHO Cloning: %_git%
+FOR %%A IN ("%_git%") DO SET _folder=%%~dpA
+SET _folder=%_folder:~0,-1%
+FOR %%A IN ("%_folder%") DO SET _folder=%%~nxA
+IF EXIST "%_folder%" (
+  git rm -r -f "%_folder%" > nul
+  git rm --cached "%_folder%" > nul
+  IF EXIST "%_folder%" RD /S /Q "%_folder%" > nul
+)
+git clone %_git% "%_folder%"
+RD /S /Q "%_folder%\.git"
+icacls "%_folder%" /setowner Everyone /C /T /L > nul
+attrib -a -i "%_folder%" /S /D /L
+git add "%_folder%" > nul
+ECHO.
+GOTO :EOF
